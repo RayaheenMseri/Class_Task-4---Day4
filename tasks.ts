66ffe61094779a9d737e202b4c1010fe58123263
Rayaheen <RayaheenMseri@gmail.com>
2025-02-20 16:01:03 +0300

    comment

--- tasks.ts
+++ tasks.ts
@@ -1,3 +1,5 @@
+//Section 1: Type Annotations & Functions
+//Task 1: Implement a Utility Function
 function capitalizeWords(text: string):
     string {
     if (text == '') {
@@ -9,7 +11,7 @@ function capitalizeWords(text: string):
 console.log(capitalizeWords("rayaheen"))
 console.log(capitalizeWords(""))
 
-
+//Task 2: Number Validator Function
 function isValidNumber(num: unknown):
     boolean {
     if (typeof (num) == `number`) {
@@ -23,7 +25,8 @@ function isValidNumber(num: unknown):
 console.log(isValidNumber(3))
 console.log(isValidNumber(""))
 
-
+//Section 2: Interfaces & Object Types
+//Task 3: Create a User System
 interface MyUser {
     id: number;
     name: string;
@@ -45,6 +48,9 @@ function createUser(user: MyUser): string {
 
 console.log(createUser({ name: "Rayaheen", id: 1, email: "rayaheen@gmail.com", idAdmin: true, }))
 
+
+//Section 3: Classes & OOP
+//Task 4: Implement a Product Class
 class Product {
     name: string;
     price: number;
@@ -65,6 +71,9 @@ class Product {
 const prod: Product = new Product('Lipstick', 500, 'Beaty');
 console.log(`the original price is : ${prod.price} , the price after discount is : ${prod.getDiscountedPrice(150)}`)
 
+
+//Section 4: Generics & Utility Types
+//Task 5: Implement a Generic Function
 function filterArray<T>(arr: T[], predicate: (item: T) => boolean): T[] {
     const result: T[] = [];
     for (const item of arr) {
@@ -79,6 +88,9 @@ const numbers = [1, 2, 3, 4, 5, 6];
 const evenNumbers = filterArray(numbers, (num) => num % 2 === 0);
 console.log(`Even numbers is : ${evenNumbers}`);
 
+
+//Section 5: TypeScript with Async/Await
+//Task 6: Fetch & Display Users
 async function getUsers(): Promise<{ id: number; name: string; email: string }[]> {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
